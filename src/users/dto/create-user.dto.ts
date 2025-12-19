2c2b231af648d7c50d5074b8079723884cbc0f8a
juniorbonini <juniorboninia7x@gmail.com>
2025-12-19 13:38:53 -0300

    CreateUserDTO - refatorado

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
